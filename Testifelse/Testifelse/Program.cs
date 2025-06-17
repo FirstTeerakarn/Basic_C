55bbd45895f174f61bb6ca763f074566ac1d6470
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-06-17 19:46:21 +0700

    Add files via upload

---
+++ Testifelse/Testifelse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testifelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dog";
+
+            if (name == "First")
+            {   //คำสั่งต่างๆ
+                Console.WriteLine("Hello : Teerakarn");
+            }
+
+            else
+            {   //คำสั่งต่างๆ
+                Console.WriteLine("Hello User : {0}", name);
+            }
+            Console.WriteLine("End Program");
+            Console.ReadKey();
+        }
+    }
+}
