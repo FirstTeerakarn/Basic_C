8e4b57a3d9fef929f13575f7e717abc437faa131
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-06-19 01:26:46 +0700

    Add files via upload

---
+++ GPA/GPA/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int score;
+            Console.WriteLine("Input Score :");
+            int.TryParse(Console.ReadLine(), out score);
+
+            //Process & Output
+            if (score >= 80)
+            {
+                Console.WriteLine("Grade A");
+            }else if (score >=70)
+            {
+                Console.WriteLine("Grade B");
+            }else if (score >= 60)
+            {
+                Console.WriteLine("Grade C");
+            }else if (score >= 50)
+            {
+                Console.WriteLine("Grade D");
+            }else
+            {
+                Console.WriteLine("Grade F");
+            }
+            Console.ReadKey();
+        }
+    }
+}
