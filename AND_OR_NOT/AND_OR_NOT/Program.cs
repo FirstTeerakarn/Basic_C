6647ad866322fe1a05f982f9137c8b837248555b
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-06-19 17:37:11 +0700

    Add files via upload

---
+++ AND_OR_NOT/AND_OR_NOT/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AND_OR_NOT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            Console.Write("Enter gender (male/female) : ");
+            string gender = Console.ReadLine();
+
+            Console.Write("Enter height : ");
+            int height = int.Parse(Console.ReadLine());
+
+            //Process & Output
+            //AND และ
+            if (gender == "male" && height >= 160)
+            {
+                Console.WriteLine("Pass");
+            }else
+            {
+                Console.WriteLine("Not Pass");
+            }
+
+
+            /* //OR หรือ
+            if (gender == "male" || height >= 160)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Not Pass");
+            }
+            */
+
+
+            /* //NOT ตรงกันข้าม
+            if (!(height >= 160)) //true => false
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Not Pass");
+            }
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
