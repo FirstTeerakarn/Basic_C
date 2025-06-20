4f9afdbdbd6ba35a503f210ec6ec6852bd9544f7
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-06-20 16:09:07 +0700

    Add files via upload

---
+++ Ternary_Operator/Ternary_Operator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Score;
+            string result;
+
+            Console.Write("Input Score : ");
+            int.TryParse(Console.ReadLine(), out Score);
+
+            result = (Score >= 50) ? "Pass" : "Not Pass";
+
+            Console.Write("Status : {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
