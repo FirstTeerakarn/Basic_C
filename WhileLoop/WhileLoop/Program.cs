5e1528d630d5fa6fb355cb79f13375cd95d2b90a
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-06-21 19:46:51 +0700

    Add files via upload

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Input Number : ");
+            int.TryParse(Console.ReadLine(), out number);
+
+            //While Loop
+            /*int count = 1;
+            while (count <= 12)
+            {
+                //คำสั่งที่ต้องการให้ทำซ้ำ
+                Console.WriteLine("{0} x {1} = {2}", number, count, number*count);
+                count++;
+            }
+            */
+
+
+            //For Loop
+            for (int count = 1; count <= 12; count++)
+            {
+                //คำสั่งที่ต้องการให้ทำซ้ำ
+                Console.WriteLine("{0} x {1} = {2}", number, count, number * count);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ if_if/if_if/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+
+            Console.Write("Input Money : ");
+            int.TryParse(Console.ReadLine(), out money);
+
+            if (money >= 15000)
+            {
+                Console.WriteLine("Sale");
+                if (money >= 15000 && money <= 19999)
+                { 
+                    Console.WriteLine("Sale = 10%");
+                }
+                else if (money >= 20000 && money <= 29999)
+                {
+                    Console.WriteLine("Sale = 20%");
+                }
+                else if (money >= 30000)
+                {
+                    Console.WriteLine("Sale = 30%");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Not Sale");
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
