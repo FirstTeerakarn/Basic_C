fe5c7632838729f1147513a869e47d6dad5396c6
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-06-21 19:47:16 +0700

    Add files via upload

---
+++ break_continue/Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter Number : ");
+            int.TryParse(Console.ReadLine(), out number);
+
+            for (int count = 1; count <= 12; count++)
+            {
+                if(count  == 5)
+                {
+                    //break; // ถึงรอบที่ 5 เมื่อไหร่ให้ออกจาก loop ทันที
+                    continue; //เมื่อ count มีค่า =5 กระโดดข้าม 5 ไปทำloopถัดไป
+                }
+                Console.WriteLine("{0} x {1} = {2}", number, count, number * count);
+            }
+            Console.WriteLine("End Program");
+            Console.ReadKey();
+        }
+    }
+}
